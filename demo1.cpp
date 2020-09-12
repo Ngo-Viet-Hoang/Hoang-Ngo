ea69c41349def59f597b065cb030819208fd3c66
Ngo-Viet-Hoang <ngoviethoang007@gmail.com>
2020-09-12 08:47:31 +0700

    Create demo1.cpp

---
+++ demo1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+	
+ 	 int n;
+ 	printf("Nhap n=");
+ 	 scanf("%d",&n);
+ 	  
+ 	if(n>0){
+	    printf("n la so duong");
+	}else{
+		printf("n las so am");
+	}
+	
+}
